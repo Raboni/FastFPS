eb2af3dc460f051c1fff2216fc22a6977e1d7534
LordFerge98 <oscarsvahn98@gmail.com>
2016-01-27 13:40:54 +0100

    Hud Update
    hud script added

---
+++ FastFPS/Assets/Scripts/HudScript.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HudScript : MonoBehaviour {
+	GUIText myText;
+	int HitPoints = 100;
+    int Armor = 100;
+	// Use this for initialization
+	void Start () {
+		myText.text = HitPoints.ToString ();
+        myText.text = Armor.ToString();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
